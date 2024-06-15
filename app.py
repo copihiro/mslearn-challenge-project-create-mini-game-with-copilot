8a5d18f91f7f138f972531d7e71ede3c47a1a5e1
copihiro <hata.takahiro@outlook.com>
2024-06-15 16:52:40 +0000

    feature: display score

--- app.py
+++ app.py
@@ -5,47 +5,55 @@ print("Welcome to Rock, Paper, Scissors Game!")
 print("You will play against computer.")
 print("Rock beats scissors. Scissors beats paper. Paper beats rock.")
 
-# Get user's choice
-user_choice = input("Enter rock, paper, or scissors: ").lower()
+# Initialize scores
+user_score = 0
+computer_score = 0
 
-# Get computer's choice
-choices = ["rock", "paper", "scissors"]
-computer_choice = random.choice(choices)
+while True:
+    # Get user's choice
+    user_choice = input("Enter rock, paper, or scissors (or 'exit' to quit): ").lower()
+    if user_choice == 'exit':
+        break
 
-# Print computer's choice
-print(f"Computer chose: {computer_choice}")
+    # Get computer's choice
+    choices = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(choices)
 
-# Determine the winner
-if user_choice == computer_choice:
-    print("It's a tie!")
-elif user_choice == "rock":
-    if computer_choice == "scissors":
-        print("You win!")
+    # Print computer's choice
+    print(f"Computer chose: {computer_choice}")
+
+    # Determine the winner and update scores
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif user_choice == "rock":
+        if computer_choice == "scissors":
+            print("You win!")
+            user_score += 1
+        else:
+            print("Computer wins!")
+            computer_score += 1
+    elif user_choice == "paper":
+        if computer_choice == "rock":
+            print("You win!")
+            user_score += 1
+        else:
+            print("Computer wins!")
+            computer_score += 1
+    elif user_choice == "scissors":
+        if computer_choice == "paper":
+            print("You win!")
+            user_score += 1
+        else:
+            print("Computer wins!")
+            computer_score += 1
     else:
-        print("Computer wins!")
-elif user_choice == "paper":
-    if computer_choice == "rock":
-        print("You win!")
-    else:
-        print("Computer wins!")
-elif user_choice == "scissors":
-    if computer_choice == "paper":
-        print("You win!")
-    else:
-        print("Computer wins!")
+        print("Invalid input! You have not entered rock, paper, or scissors, try again.")
+
+# Print final scores
+print(f"Final Scores:\nYou: {user_score}\nComputer: {computer_score}")
+if user_score > computer_score:
+    print("You are the overall winner!")
+elif user_score < computer_score:
+    print("Computer is the overall winner!")
 else:
-    print("Invalid input! You have not entered rock, paper, or scissors, try again.")
-    exit()
-
-# Ask user to play again
-play_again = input("Do you want to play again? (yes/no): ").lower()
-if play_again != "yes":
-    # End of the game
-    print("Thanks for playing Rock, Paper, Scissors Game!")
-    exit()
-else:
-    # Play again
-    print("Let's play again!")
-    print()
-    # Run the game again
-    exec(open("app.py").read())
+    print("It's a tie overall!")
